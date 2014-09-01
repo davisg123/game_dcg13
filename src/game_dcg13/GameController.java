20581b639ed8544b08a656a62aad634cfdbd90b8
Davis Gossage <dcg13@duke.edu>
2014-08-31 21:52:06 -0400

    Further abstraction, the Level object now handles the drag event.

--- src/game_dcg13/GameController.java
+++ src/game_dcg13/GameController.java
@@ -1,18 +1,12 @@
 package game_dcg13;
  
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
  
 public class GameController extends Application {
-	private static Laser laserLine;				///the current line that represents the laser beam
 	private static Level currentLevel;
 	
     public static void main(String[] args) {
@@ -26,29 +20,10 @@ public class GameController extends Application {
         Pane root = new Pane();
 
         currentLevel = new Level(root);
-        handleDragOnPane(root);
         
         primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
-    
-    private static void handleDragOnPane(Pane root){
-    	root.onMouseDraggedProperty().set(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-            	if (laserLine != null){
-            		//remove the previous laser
-            		laserLine.remove();
-            	}
-            	if (laserLine != null && laserLine.intersects(laserLine.getLayoutBounds())){
-            		
-            	}
-            	//generate a laser from mirror man's static location to the click location
-                laserLine = new Laser(150,270,(int)event.getX(),(int)event.getY(),true,root);
-            }
-        });
-    }
-    
     
 
 }
--- src/game_dcg13/Level.java
+++ src/game_dcg13/Level.java
@@ -1,10 +1,13 @@
 package game_dcg13;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class Level {
+	private static Laser laserLine;				///the current line that represents the laser beam
 	private Pane rootPane;
 	
 	public Level(Pane root){
@@ -16,6 +19,8 @@ public class Level {
         generateLine(125,270,175,270,true,root);
         generateLine(30,270,125,270,false,root);
         generateLine(175,270,270,270,false,root);
+        //detect drag event
+        handleDragOnPane(rootPane);
 	}
 	
     private static Line generateLine(int startX, int startY, int endX, int endY, Boolean isDashed, Pane root){
@@ -28,5 +33,23 @@ public class Level {
         }
         root.getChildren().add(aLine);
         return aLine;
+    }
+    
+    
+    private static void handleDragOnPane(Pane root){
+    	root.onMouseDraggedProperty().set(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+            	if (laserLine != null){
+            		//remove the previous laser
+            		laserLine.remove();
+            	}
+            	if (laserLine != null && laserLine.intersects(laserLine.getLayoutBounds())){
+            		
+            	}
+            	//generate a laser from mirror man's static location to the click location
+                laserLine = new Laser(150,270,(int)event.getX(),(int)event.getY(),true,root);
+            }
+        });
     }
 }
